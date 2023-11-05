c543709cfbff2c34ebc5ad79615e655be8a4b34e
Daphyk <kendid07gmail.com>
2023-11-05 16:26:41 +0300

    Completed Assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,12 +4,15 @@ import Comment from "./Comment";
 
 function BlogPost() {
   return (
-    <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
-    </div>
+    <div> 
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+  
+    <div>I agree with this statement. - Angela Merkel</div>
+  
+    <div>A universal truth. - Noam Chomsky</div>
+  
+    <div>Truth is singular. Its ‘versions’ are mistruths - Sonmi-451</div>
+  </div>
   );
 }
 
